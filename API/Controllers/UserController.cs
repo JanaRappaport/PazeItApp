d0fd126b096f23628f57e5e3ae30adb8169dd79c
hodaya.farkash@gmail.com <hodaya.farkash@gmail.com>
2022-07-18 19:43:13 +0300

    Add project files.

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace API.Controllers
+{
+    [EnableCors("*","*","*")]
+    [RoutePrefix("api/user")]
+    public class UserController : ApiController
+    {
+        
+        [Route("GetNumOfUsers")]
+        [HttpGet]
+        public IHttpActionResult GetNumOfUsers()
+        {
+            return Ok(1);
+        }
+
+        [Route("GetNumOfproduct/{numOfP}")]
+        [HttpGet]
+        public int GetNumOfproduct(int numOfP)
+        {
+            return numOfP;
+        }
+
+       [ Route("AddUser"), HttpPost]
+        public IHttpActionResult AddUser(userDTO user)
+        {
+            BL.userBL.AddUser(user);
+            return Ok(true);
+
+        }
+
+    }
+}
---
+++ BL/userBL.cs
@@ -0,0 +1,23 @@
+﻿
+using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+   public class userBL
+    {
+        public static void AddUser(userDTO user)
+        {
+            using (PITdataBaseEntities DB = new PITdataBaseEntities())
+            {
+                DB.users.Add(converters.userConverter.Map(user));
+                DB.SaveChanges();
+            }
+        }
+    }
+}
